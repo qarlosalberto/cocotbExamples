f6c8dfa02b2747dc3755202aa99f58c5c0a34bbc
qarlosalberto <carlosruiznaranjo@gmail.com>
2019-08-22 01:45:43 +0200

    end.

--- tools/cocotb/scoreboard_simple/test_adder.py
+++ tools/cocotb/scoreboard_simple/test_adder.py
@@ -10,24 +10,24 @@ from cocotb.scoreboard import Scoreboard
 from cocotb.binary import BinaryValue
 
 # ==============================================================================
-# class InputMonitor(Monitor):
-#     """Observes a single-bit input or output of DUT."""
-#     def __init__(self, name, signala, signalb, clock, callback=None, event=None):
-#         self.name = name
-#         self.signala = signala
-#         self.signalb = signalb
-#         self.clock = clock
-#         Monitor.__init__(self, callback, event)
-#
-#     @cocotb.coroutine
-#     def _monitor_recv(self):
-#         clkedge = RisingEdge(self.clock)
-#
-#         while True:
-#             # Capture signal at rising edge of clock
-#             yield clkedge
-#             vec = self.signala.value
-#             self._recv(vec)
+class InputMonitor(Monitor):
+    """Observes a single-bit input or output of DUT."""
+    def __init__(self, name, signala, signalb, clock, callback=None, event=None):
+        self.name = name
+        self.signala = signala
+        self.signalb = signalb
+        self.clock = clock
+        Monitor.__init__(self, callback, event)
+
+    @cocotb.coroutine
+    def _monitor_recv(self):
+        clkedge = RisingEdge(self.clock)
+
+        while True:
+            # Capture signal at rising edge of clock
+            yield clkedge
+            vec = [self.signala.value,self.signalb.value]
+            self._recv(vec)
 # ==============================================================================
 class OutMonitor(Monitor):
     """Observes a single-bit input or output of DUT."""
@@ -43,17 +43,10 @@ class OutMonitor(Monitor):
 
         while True:
             # Capture signal at rising edge of clock
-            print("captura")
             yield clkedge
             vec = self.signal.value
             self._recv(vec)
 # ==============================================================================
-
-
-
-# ==============================================================================
-
-
 @cocotb.coroutine
 def gen_clk(clk, period):
     while True:
@@ -62,52 +55,37 @@ def gen_clk(clk, period):
         clk.value = 1
         yield Timer(period/2)
 
-# Create a scoreboard on the outputs
+class TB(object):
+    def __init__(self, dut):
+        self.dut = dut
+        # Reconstruct the input transactions from the pins
+        # and send them to our 'model'
+        input_mon = InputMonitor("input", dut.A, dut.B, dut.clk,callback=self.adder_modelD)
+        # Output monitor
+        self.output_mon = OutMonitor("output", dut.X, dut.clk)
+        # Create a scoreboard on the outputs
+        self.expected_output = []
+        self.scoreboard = Scoreboard(dut)
+        self.scoreboard.add_interface(self.output_mon, self.expected_output)
 
+    def adder_modelD(self,transaction):
+        result = adder_model(transaction[0],transaction[1])
+        self.expected_output.append(BinaryValue(value=result,n_bits=int(self.dut.DATA_WIDTH),bigEndian=False))
 
-
-
-
-
-class TB(object)
-    def __init__(self, dut, init_val):
-
-
-
-
-
-
-expected_output = [BinaryValue(value=30,n_bits=int(20),bigEndian=False)]
 @cocotb.test()
-def adder_random_test(dut):
+def run_test(dut):
+    #Setup test
     PERIOD = 10
     clk = dut.clk
     cocotb.fork(gen_clk(clk, PERIOD))
 
-    # Output monitor
-    output_mon = OutMonitor("output", dut.X, dut.clk)
+    tb = TB(dut)
 
-    # # Create a scoreboard on the outputs
-    # expected_output = []
-    cocotb.scoreboard = Scoreboard(dut)
-    cocotb.scoreboard.add_interface(output_mon, expected_output)
-    # Reconstruct the input transactions from the pins
-    # and send them to our 'model'
-    def adder_modelD(self):
-        print("entra")
-        expected_output.append(BinaryValue(value=30,n_bits=int(dut.DATA_WIDTH),bigEndian=False))
-
-    input_mon = OutMonitor("input", dut.A, dut.clk,callback=adder_modelD)
-
-    for i in range(3):
-        A = 10
-        B = 20
-        dut.A = A
-        dut.B = B
+    for i in range(100):
+        dut.A = random.randint(0,10)
+        dut.B = random.randint(0,10)
 
         yield Timer(1*PERIOD)
 
-    print(expected_output)
-
     # Print result of scoreboard.
-    # raise cocotb.scoreboard.result
+    raise tb.scoreboard.result
